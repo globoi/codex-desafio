b8702c34a971da7fecf666cfa230be6534408aa4
Antônio Luiz Bretas Peixoto <70115251+AntonioBretas@users.noreply.github.com>
2025-05-15 22:09:06 -0300

    refactor: pequenas alterações no tamanho do video em VideoModal.

--- src/VideoModal.jsx
+++ src/VideoModal.jsx
@@ -1,4 +1,5 @@
 import ReactModal from "react-modal";
+import './styles/VideoModal.css';
 
 ReactModal.setAppElement('#root');
 
@@ -11,15 +12,21 @@ function  VideoModal({isOpen, onClose, videoUrl}) {
             shouldCloseOnOverlayClick={true}
             style={{
             overlay: { backgroundColor: 'rgba(0, 0, 0, 0.5)' },
-            content: { margin: 'auto', width: '50%', padding: '2rem' },
+            content: { 
+                margin: 'auto', 
+                width: 'auto', 
+                maxWidth: '90%',
+                padding: '2rem', 
+                background:'none', 
+                border: 'none', 
+                overflow:'hidden' 
+            },
             }}
         >
-            <h2>Vídeo</h2>
-            <video controls width="100%">
+            <video className="video-modal" controls width="100%">
                 <source src={videoUrl} type="video/mp4" />
                 Seu navegador não suporta o elemento de vídeo.
             </video>
-            <button onClick={onClose}>Fechar</button>
         </ReactModal>
         </>
     );
